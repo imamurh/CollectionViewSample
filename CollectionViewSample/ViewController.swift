81ca1494a5e07b8ea15f31ddf586527468ea8db5
imamurh <imamurh@gmail.com>
2018-12-02 20:46:48 +0900

    Initial Commit

---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by Hajime Imamura on 2018/12/02.
+//  Copyright © 2018 imamurh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
